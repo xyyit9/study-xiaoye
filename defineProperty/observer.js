99153710df6947c96c7721407a79c6666845c798
alina <mixiu323@qq.com>
2021-01-06 20:54:57 +0800

    defineProperty

---
+++ defineProperty/observer.js
@@ -0,0 +1,7 @@
+export function observer(userInfo, viewDom) {
+  var _strogeInfo = JSON.parse(localStorage.getItem('userInfo')) || '{}'
+  var _retInfo = {};
+  var init = function () {}
+  
+  init()
+}
